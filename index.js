8be78afbb81245cec257b01cb6658631d05c4975
Copitonaldo <jsjdhjdjdj22@gmail.com>
2025-06-27 00:23:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Importar Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js ";
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js ";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
 // Tu configuración de Firebase
 const firebaseConfig = {
